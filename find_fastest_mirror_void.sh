457456a170248a3086aec941461ffc3313fda335
cslycord <cslycord@gmail.com>
2020-01-16 15:21:32 +0900

    Fixed output for long timeouts and added verbosity

--- find_fastest_mirror_void.sh
+++ find_fastest_mirror_void.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-repos="http://alpha.de.repo.voidlinux.org http://beta.de.repo.voidlinux.org \
+repos="http://alpha.de.repo.voidlinux.org \
+http://beta.de.repo.voidlinux.org \
 http://alpha.us.repo.voidlinux.org \
 http://mirror.clarkson.edu/voidlinux/ \
 http://mirrors.servercentral.com/voidlinux/ \
@@ -10,10 +11,33 @@ http://ftp.acc.umu.se/mirror/voidlinux.eu/ \
 https://mirrors.dotsrc.org/voidlinux/ \
 http://www.gtlib.gatech.edu/pub/VoidLinux/ \
 https://void.webconverger.org \
+https://youngjin.io/voidlinux \
 http://ftp.lysator.liu.se/pub/voidlinux/ \
 http://lysator7eknrfl47rlyxvgeamrv7ucefgrrlhk7rouv3sna25asetwid.onion/pub/voidlinux/"
 
 min_repo=""
+
+commands_exist() {
+	check_cmds=$*
+
+	if command -v type >/dev/null ; then
+		type ${check_cmds} >/dev/null
+	else
+		for cmd in ${check_cmds}; do
+			if ! command -v "$cmd" >/dev/null ; then
+				return 1
+			fi
+		done
+	fi
+}
+
+needed_cmds="awk sed ping tail cut"
+
+if ! commands_exist ${needed_cmds} ; then
+	echo "Your system is missing needed packages"
+	echo "It needs $needed_cmds"
+	exit 1
+fi
 
 smaller() {
 	new_min=$1
@@ -24,16 +48,28 @@ smaller() {
 	return 1
 }
 
+if [ "$1" = "--verbose" ]; then
+	verbose="true"
+fi
+
 for repo in ${repos}; do
 	host=$(echo $repo | sed -e 's|^http://*||g' -e 's:/.*::')
-	ping=$(ping -c 4 -w 400 $host 2>/dev/null | tail -1 | awk '{print $4}' \
+	pingt=$(ping -c 4 -w 2 $host 2>/dev/null | tail -1 | awk '{print $4}' \
 	| cut -d '/' -f 2)
-	if [ "$ping" != "" ] && [ "$ping" != "0" ]; then
-		if [ "$min_ping" = "" ] || smaller $ping $min_ping ; then
-			min_ping="$ping"
+
+	if [ "$pingt" != "" ]; then
+		if [ "$min_ping" = "" ] || smaller $pingt $min_ping ; then
+			min_ping="$pingt"
 			min_repo="$repo"
 		fi
+	else
+		pingt="timed out"
 	fi
+
+	if [ "$verbose" = "true" ];then
+		echo "$repo: $pingt"
+	fi
+
 done
 if [ "$min_repo" != "" ]; then
 	echo "$min_repo"
